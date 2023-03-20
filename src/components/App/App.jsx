6ef9d76c67c124b9c3d93d42129d955a4bd462f9
Ong Heng Kiat <onghengkiat105@gmail.com>
2023-03-20 19:38:41 +0800

    Remove the extra user table used for testing

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -80,7 +80,7 @@ function App() {
     }
   ]);
   return (
-    <ThemeProvider theme={theme}><User setSnackbarMessage={setSnackbarMessage} setLoading={setLoading}/>
+    <ThemeProvider theme={theme}>
       <GlobalStyles />
       <NotificationPopup setSnackbarMessage={setSnackbarMessage} snackbarMessage={snackbarMessage} />
       <LoadingSpinner loading={loading}/>
