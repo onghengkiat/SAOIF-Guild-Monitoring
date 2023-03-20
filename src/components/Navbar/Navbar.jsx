988be0a7fabaa3313110f35564e20fbcb1b27a47
Ong Heng Kiat <onghengkiat105@gmail.com>
2023-03-20 21:33:33 +0800

    Change the UI for the navigation bar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -8,11 +8,10 @@ import Divider from '@mui/material/Divider';
 import Typography from '@mui/material/Typography';
 import { styled } from '@mui/material/styles';
 import Box from '@mui/material/Box';
+import MenuIcon from '@mui/icons-material/Menu';
 
-import { 
-    Drawer,
-    AppBar,
-} from './Drawer';
+import Drawer from '@mui/material/Drawer';
+import MuiAppBar from '@mui/material/AppBar';
 
 import {
     NavbarPrimaryItems, 
@@ -22,55 +21,48 @@ import {
 const BoxOutsideNavbar = styled(Box)(({theme}) => ({
     flexGrow: 1,
     height: '100vh',
+    paddingTop: `${APPBAR_HEIGHT}px`,
     overflow: 'auto',
     background: theme.palette.mode === 'light' ? theme.palette.grey[200] : theme.palette.grey[700],
 }));
 
+const APPBAR_HEIGHT = 64;
+
 export default function Navbar({ activePage, children, token, setToken, setSnackbarMessage }) {
-    const [open, setOpen] = useState(true);
-    const toggleDrawer = () => {
-      setOpen(!open);
-    };
+    const [open, setOpen] = useState(false);
 
     return (
-        <Box sx={{ display: 'flex' }}>
-            <AppBar position="absolute" open={open}>
+        <div>
+            <MuiAppBar position="absolute" sx={{height: `${APPBAR_HEIGHT}px`}}>
                 <Toolbar>
+                    <IconButton
+                    edge="start"
+                    color="inherit"
+                    aria-label="menu"
+                    onClick={() => setOpen(true)}
+                    >
+                        <MenuIcon />
+                    </IconButton>
                     <Typography
                         component="h1"
                         variant="h6"
                         color="inherit"
                         noWrap
-                        sx={{ flexGrow: 1 }}
                     >
                         {activePage}
                     </Typography>
                 </Toolbar>
-            </AppBar>
-            <Drawer variant="permanent" open={open}>
-                <Toolbar
-                    sx={{
-                        display: 'flex',
-                        alignItems: 'center',
-                        justifyContent: 'flex-end',
-                        px: [1],
-                    }}
-                >
-                    <IconButton onClick={toggleDrawer}>
-                        { open ? <ChevronLeftIcon /> : <ChevronRightIcon />}
-                    </IconButton>
-                 </Toolbar>
-                <Divider />
-                <List component="nav">
-                    <NavbarPrimaryItems activeItem={activePage} token={token}/>
-                    <Divider sx={{ my: 1 }} />
-                    <NavbarSecondaryItems activeItem={activePage} setToken={setToken} setSnackbarMessage={setSnackbarMessage}/>
-                </List>
-            </Drawer>
+                <Drawer open={open} anchor="top" onClose={() => setOpen(false)}>
+                    <List component="nav">
+                        <NavbarPrimaryItems activeItem={activePage} token={token}/>
+                        <Divider sx={{ my: 1 }} />
+                        <NavbarSecondaryItems activeItem={activePage} setToken={setToken} setSnackbarMessage={setSnackbarMessage}/>
+                    </List>
+                </Drawer>
+            </MuiAppBar>
             <BoxOutsideNavbar>
-                <Toolbar />
                 {children}
             </BoxOutsideNavbar>
-      </Box>
+      </div>
     )
 }
